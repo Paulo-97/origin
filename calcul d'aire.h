ded348c384478c3645d891105b4b20d43c3dd933
Paulo-97 <mr.paulos97@gmail.com>
2019-03-18 19:27:03 +0100

    Initial commit

---
+++ calcul d'aire.h
@@ -0,0 +1,14 @@
+/*pogramme de calcul d'aire*/
+
+#include <stdio.h>
+#include <math.h>
+
+main()
+{
+    float pi=3.14159;
+    float rayon, surface;
+    scaf ("%f", &rayon);
+    surface:=pi*pow(raion*raion);
+    printf("%f\", surface);
+    return 0;
+    }
